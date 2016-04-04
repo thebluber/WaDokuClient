ef9787fb10c4e79ec47d722de0654067bd18e6d9
Jiayi Zheng <thebluber@gmail.com>
2016-04-04 22:31:47 +0200

    Add IME with kvg_character_recognition for WaDokuClient Update bootstrap, jquery and lodash Add fontawesome

---
+++ public/js/ime.js
@@ -0,0 +1,33 @@
+'use strict';
+/*jslint browser: true*/
+/*global $, jQuery, alert, utils, api, desktop, mobile*/
+
+function init() {
+  var canvas = document.getElementById("pad");
+  var buttons = {
+    cleanAll: document.getElementById("cleanAll"),
+    cleanLast: document.getElementById("cleanLast"),
+    save: document.getElementById("save")
+  };
+  if(canvas.getContext){
+    if(screen.width <= 800){
+      mobile(canvas, buttons);
+    } else {
+      desktop(canvas, buttons);
+    }
+  } else {
+    alert("Your browser does not support canvas!");
+  }
+}
+//$( document ).ready(init);
+document.addEventListener( "DOMContentLoaded", function(){
+  // setup a new canvas for drawing wait for device init
+
+  $('#ime-toggle').click(function(){
+    $('#ime').toggle(function() {
+      init();
+    });
+    $('.buttons').toggle();
+  });
+}, false );
+
